dadef30310f220d59b55d08b80e95892bf43e66a
yonmoyonmo <yonmosound@gmail.com>
2021-07-28 17:51:10 +0900

    wcs-vchat

--- server.js
+++ server.js
@@ -4,14 +4,22 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
+
+app.get('/', (req,res) =>{
+  res.redirect(`/vchat/${uuidV4()}`)
 })
 
-app.get('/:room', (req, res) => {
+app.get('/vchat', (req, res) => {
+  res.redirect(`/vchat/${uuidV4()}`)
+})
+
+app.get('/vchat/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
 })
 
